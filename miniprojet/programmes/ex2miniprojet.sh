fb9babff7b5035f458b0def6ee843d3f2871baee
marine.yang@sorbonne-nouvelle.fr <marine.yang@sorbonne-nouvelle.fr>
2025-10-30 15:11:19 +0100

    commit maj pour le tag nouveau

--- miniprojet/programmes/ex2miniprojet.sh
+++ miniprojet/programmes/ex2miniprojet.sh
@@ -1,25 +1,14 @@
  #!/bin/bash
 
-
-line="/home/marine/Documents/Plurital/gital/PPE1-2025/miniprojet/urls/fr.txt"
-N=1
-output="/home/marine/Documents/Plurital/gital/PPE1-2025/miniprojet/tableaux/tableau-fr.tsv"
-
-echo "le fichier est : $line"
-    echo -e "$N \t"
-
+    {
+    i=1
     while read -r line;
 do
-    echo -e "$N\t ${line}"
-    encodage=$(curl -s -I "$line" | grep -i "charset=UTF-8")
+    encodage=$(curl -s -I "$line" | grep -o -i "charset=UTF-8" | tr -d '\n')
     code=$(curl -s -I "$line" | grep -i "HTTP/")
 
-    echo -e "$line\t$encodage\t$code"
-
-    result=$(echo -e "$line\t$encodage\t$code");
-
-	echo "$result" >> "$output"
-
-    N=$((N + 1))
-done <"$line"
+    echo -e "${i}'\t'${code}'\t'${line}'\t'${encodage}" >> ../tableaux/tableau-fr.tsv
+    ((i++))
+done <"../urls/fr.txt"
+}
 
